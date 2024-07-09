28e09d66bd2ac48abd620206c8754c4ff41d4480
William <william_7527@hotmail.com>
2024-07-09 11:37:00 -0500

    Agregar scroll en botones de encriptar/desencriptar y actualización en CSS y README

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ function encryptText() {
         text = text.replaceAll(keysArray[i], valuesArray[i]); // Reemplazar llaves por valores en el texto
     }
     outputText.textContent = text; // Mostrar texto encriptado en la salida
+    document.querySelector('.result').scrollIntoView({ behavior: 'smooth' });
     checkInputText(); // Verificar y Actualizar visualización de resultados
 }
 
@@ -37,6 +38,7 @@ function decryptText() {
         text = text.replaceAll(valuesArray[i], keysArray[i]); // Reemplazar valores por llaves en el texto
     }
     outputText.textContent = text; // Mostrar texto desencriptado en la salida
+    outputText.scrollIntoView({ behavior: 'smooth' });
     checkInputText(); // Verificar y Actualizar visualización de resultados
 }
 
